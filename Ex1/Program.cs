479c4b56c94e03f940a90f907a7e81ba6ba66d9f
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-18 13:29:50 +0300

    add Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+void FindStep (int a, int b)
+ {
+    int resalt = 1;
+    for (int i = 0; i < b; i++)
+    {
+        resalt *= a;
+    }
+     Console.WriteLine (resalt);
+ }
+ FindStep(3,5);
