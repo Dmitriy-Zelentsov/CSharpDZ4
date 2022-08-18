63ba078ba110d853caa6334890d29dd3cfbc925c
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-18 19:59:08 +0300

    add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+void FindSum (int num)
+{
+string num_tostring = num.ToString();
+
+Console.WriteLine(num_tostring);
+}
+FindSum (9012);
